edd049353b0cb0d38f544a2c5edb000c7e6b930e
xeratun <nutarex@gmail.com>
2017-05-03 20:37:05 -0400

    update

--- OAuth-Implementation/oauth.py
+++ OAuth-Implementation/oauth.py
@@ -1,47 +1,59 @@
-#import requests
-#import requests.auth
+CLIENT_ID = "822545719705-ai33lpgvdtj7o069r4krikae6s20r663.apps.googleusercontent.com"
+CLIENT_SECRET = "XjwySGyHwuhOvAKkGMAPtZ_z"
+GOOGLE_URL = "https://accounts.google.com/o/oauth2/v2/auth"
+REDIRECT_URL = "https://oauthimplementation-166521.appspot.com/callback"
+state = ""
+
 import os
-import urllib
+from google.appengine.api import urlfetch
 from google.appengine.api import users
 from google.appengine.ext import ndb
 import jinja2
 import webapp2
 import json
+import string
+import random
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+def generate_state(length=10, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(length))
+
 class CallbackHandler(webapp2.RequestHandler):
-	def get(self):
-		guestbook_name = self.request.get('guestbook_name',
-										  DEFAULT_GUESTBOOK_NAME)
-		greetings_query = Greeting.query(
-			ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-		greetings = greetings_query.fetch(10)
+	def get(self,):
+		state_return = self.request.get('state')
+		code = self.request.get('code')
 		
-		user = users.get_current_user()
-		if user:
-			url = users.create_logout_url(self.request.uri)
-			url_linktext = 'Logout'
-		else:
-			url = users.create_login_url(self.request.uri)
-			url_linktext = 'Login'
+		#if state_return != state:
+			
+		
+		payload = {'grant_type': 'authorization_code',
+		'code': code,
+		'client_id': CLIENT_ID,
+		'client_secret': CLIENT_SECRET,
+		'redirect_uri': REDIRECT_URL}
+		result = json.loads(urlfetch.fetch(url='https://www.googleapis.com/oauth2/v4/token', payload = payload, method=urlfetch.POST))
+		
+		auth_key = "Bearer " + result.access_token
+		
+		payload = {'Authorization': auth_key}
+		response = json.loads(urlfetch.fetch(url='https://www.googleapis.com/plus/v1/people/me', payload = payload, method=urlfetch.GET))
 		
 		template_values = {
-			'user': user,
-			'greetings': greetings,
-			'guestbook_name': urllib.quote_plus(guestbook_name),
-			'url': url,
-			'url_linktext': url_linktext,
+			'name': response.displayName,
+			'url': response.url,
+			'state': state
 		}
 		
-		template = JINJA_ENVIRONMENT.get_template('index.html')
+		template = JINJA_ENVIRONMENT.get_template('callback.html')
 		self.response.write(template.render(template_values))
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		url = "https://accounts.google.com/o/oauth2/v2/auth"
+		state = generate_state()
+		url = GOOGLE_URL + "?response_type=code&client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URL + "&scope=email&state=" + state
 		template_values = {
 			'url': url
 		}
